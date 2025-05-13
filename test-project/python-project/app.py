a66585853ab00c9b2ef380a0ed57d769c5da0fd2
Vivekupreti02 <vickyupreti787@gmail.com>
2025-05-13 17:48:30 +0530

    action-test-app

---
+++ test-project/python-project/app.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# Only runs when executed directly
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("5 - 1 =", subtract(5, 1))
